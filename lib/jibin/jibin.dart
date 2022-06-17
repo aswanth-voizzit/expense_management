6cb3cdc27dd9b5780bb1a328fcf95e4ac80ea9bd
Jibin <jibin@voizzit.com>
2022-06-17 10:33:47 +0400

    initial commit

---
+++ lib/jibin/jibin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JibinReader extends StatefulWidget {
+  const JibinReader({Key? key}) : super(key: key);
+
+  @override
+  State<JibinReader> createState() => _JibinReaderState();
+}
+
+class _JibinReaderState extends State<JibinReader> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
